0c4ec7cb6a8a69030ae169f85a90dfafcefde48f
samuelcpr <samuelcpr464@gmail.com>
2022-04-19 16:54:06 -0300

    add past

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { api } from "../services/api";
-
+import { Button } from "./Button";
 
 interface GenreResponseProps {
   id: number;
@@ -18,3 +18,26 @@ export function SideBar({ setSelectedGenreId, selectedGenreId }: Props) {
       setGenres(response.data);
     });
   }, []);
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
+}
