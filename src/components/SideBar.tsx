c8488c1953bfb3757a57c3627462d1bed9e9f939
samuelcpr <samuelcpr464@gmail.com>
2022-04-19 16:49:02 -0300

    add

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,20 @@
-export function SideBar() {
-  // Complete aqui
+import React, { useEffect, useState } from "react";
+import { api } from "../services/api";
+
+
+interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
 }
+type Props = {
+  selectedGenreId: number;
+  setSelectedGenreId: (id: number) => void;
+};
+export function SideBar({ setSelectedGenreId, selectedGenreId }: Props) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+  useEffect(() => {
+    api.get<GenreResponseProps[]>("genres").then((response) => {
+      setGenres(response.data);
+    });
+  }, []);
